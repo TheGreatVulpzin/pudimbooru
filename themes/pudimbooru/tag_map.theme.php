b6055d6099c8e2faafd528805c41f9e9270fc468
Vulpzin <34246820+Vulpzin@users.noreply.github.com>
2026-05-20 21:52:36 -0300

    começo do tema - arroz

---
+++ themes/pudimbooru/tag_map.theme.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class pudimbooruTagMapTheme extends TagMapTheme
+{
+    protected function display_nav(): void
+    {
+        Ctx::$page->set_layout("no-left");
+        parent::display_nav();
+    }
+}
