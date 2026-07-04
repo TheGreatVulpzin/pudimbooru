119618c0171a35226fd9eb6317e207e80d24719a
Vulpzin <34246820+Vulpzin@users.noreply.github.com>
2026-07-03 22:36:22 -0300

    test: allow captcha on localhost

--- core/Util/Captcha.php
+++ core/Util/Captcha.php
@@ -14,9 +14,10 @@ final class Captcha
             return null;
         }
 
-        if (Network::get_real_ip()->is_localhost()) {
-            return null;
-        }
+        // Temporarily allow captcha rendering on localhost for Turnstile testing.
+        // if (Network::get_real_ip()->is_localhost()) {
+        //     return null;
+        // }
 
         return send_event(new BuildCaptchaEvent())->html;
     }
@@ -27,9 +28,10 @@ final class Captcha
             return true;
         }
 
-        if (Network::get_real_ip()->is_localhost()) {
-            return true;
-        }
+        // Temporarily allow captcha validation on localhost for Turnstile testing.
+        // if (Network::get_real_ip()->is_localhost()) {
+        //     return true;
+        // }
 
         $passed = send_event(new CheckCaptchaEvent())->passed;
         if ($passed === null) {
